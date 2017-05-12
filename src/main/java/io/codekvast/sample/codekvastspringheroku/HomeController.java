d4eca9e259997fc0a9b6aef08b0dc82524aaf991
perty <per@artcomputer.se>
2017-05-12 12:28:11 +0200

    A controller

---
+++ src/main/java/io/codekvast/sample/codekvastspringheroku/HomeController.java
@@ -0,0 +1,15 @@
+package io.codekvast.sample.codekvastspringheroku;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    String home() {
+        return "Hello World!";
+    }
+}

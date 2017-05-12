978147822b9f7ae60a3af8fab76fb853762d745c
perty <per@artcomputer.se>
2017-05-12 12:06:46 +0200

    Empty Spring Boot application

---
+++ src/main/java/io/codekvast/sample/codekvastspringheroku/CodekvastSampleApplication.java
@@ -0,0 +1,12 @@
+package io.codekvast.sample.codekvastspringheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodekvastSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodekvastSampleApplication.class, args);
+	}
+}
